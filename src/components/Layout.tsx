af15544846d86e63b350d447c72791540b099be1
Thomas Anderson <thomas.anderson@ucdconnect.ie>
2021-01-24 22:47:55 -0700

    Modified initial AppBar and menu nav

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,55 +2,16 @@ import Head from "next/head";
 import NavigationBar from "./NavigationBar";
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import { Fragment, useState } from "react";
-
-import Drawer from '@material-ui/core/Drawer';
 import { makeStyles } from '@material-ui/core/styles';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import Typography from '@material-ui/core/Typography';
-import Link from "next/link";
 import { useRouter } from "next/router";
 
 type Props = {
   children: React.ReactNode;
 };
 
-const useStyles = makeStyles((theme) => ({
-  title: {
-    flexGrow: 1,
-  },
-  list: {
-    width: 250,
-  },
-  fullList: {
-    width: 'auto',
-  },
-}));
-
 export default function Layout({ children }: Props) {
   const router = useRouter();
-  const classes = useStyles();
-  const [state, setState] = useState({
-    top: false,
-    left: false,
-    bottom: false,
-    right: false,
-  });
 
-  const toggleDrawer = (anchor, open) => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
-
-    setState({ ...state, [anchor]: open });
-  };
-  
   return (
     <Grid container spacing={5} xs={12}>
       <Head>
@@ -62,34 +23,12 @@ export default function Layout({ children }: Props) {
       </Head>
         <Grid container item xs={12} spacing={3}>
           <nav>
-            <NavigationBar drawerCB={(drawerStatus) => toggleDrawer('left', drawerStatus)} />
+            <NavigationBar />
           </nav>
         </Grid>
         <Grid container item xs={12} spacing={3}>
           <main>{children}</main>
-          <style jsx>
-        {`
-        `}
-      </style>
         </Grid>
-        <Drawer anchor={'left'} open={state['left']} onClose={toggleDrawer('left', false)}>
-          <Typography variant="h6" className={classes.title}>
-            <Link href="/">
-              <a className={router.pathname === "/" ? "active" : null}>about</a>
-            </Link>
-          </Typography>
-          <Typography variant="h6" className={classes.title}>
-            <Link href="/posts">
-              <a
-                className={
-                  router.pathname.startsWith("/posts") ? "active" : null
-                }
-              >
-                  blog
-              </a>
-            </Link>
-          </Typography>
-        </Drawer>
       </Grid>
   );
 }
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -7,7 +7,11 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-
+import TrainTwoToneIcon from '@material-ui/icons/TrainTwoTone';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import Link from "next/link";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,23 +26,79 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 type Props = {
-  drawerCB: Function;
+  // drawerCB: Function;
 };
 const NavigationBar: React.FunctionComponent<Props> = props => {
   const classes = useStyles();
+  const router = useRouter();
+  const [auth, setAuth] = useState(true);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <Fragment>
       <AppBar>
           <Toolbar>
             <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon onClick={props.drawerCB(true)}/>
+              <TrainTwoToneIcon /* onClick={props.drawerCB(true)}*/ />
             </IconButton>
             <Typography variant="h6" className={classes.title}>
               Title Placeholder
             </Typography>
 
-            <Button color="inherit">Login</Button>
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem>
+                  <Link href="/">
+                    <a className={router.pathname === "/" ? "active" : null}>about</a>
+                  </Link>
+                </MenuItem>
+                <MenuItem>
+                  <Typography variant="h6" className={classes.title}>
+                    <Link href="/posts">
+                      <a className={router.pathname.startsWith("/posts") ? "active" : null}>
+                          blog
+                      </a>
+                    </Link>
+                  </Typography>
+                </MenuItem>
+              </Menu>
+            </div>
           </Toolbar>
         </AppBar>
     </Fragment>
